c4a0b3347902b1166434c0d72bed249c1dab68f7
smartwe <sparklingwee@Gmail.com>
2021-03-21 16:58:49 +0900

    commit

--- jungol/arr2_selfdiagnose_4.c
+++ jungol/arr2_selfdiagnose_4.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    int arr[5][3] = {
+    int i;
+    int arr[4][2] = {
                     {5, 8, 10, 6, 4},
                     {11, 20, 1, 13, 2},
                     {7, 29, 14, 22, 3}
-                    },
-    
+                    };
+    for (i = 0; i < 5; i++)
+    {  
+       printf("%2d",arr[i][0]);
+       printf("%2d",arr[i][1]);
+       printf("%2d",arr[i][2]); 
+    }
     return 0;
 }
