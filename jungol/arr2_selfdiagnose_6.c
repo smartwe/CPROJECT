cbb5b3a0f4cc864e5302eb776da431f0bf4b029b
smartwe <sparklingwee@Gmail.com>
2021-03-22 09:56:47 +0900

    commit

--- jungol/arr2_selfdiagnose_6.c
+++ jungol/arr2_selfdiagnose_6.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
 int main()
 {
-    int class[5][4];
+    int score[5][4];
     int i, j;
     int pass;  
-    int a, b;  
+    int result1, result2, result3, result4, result5; 
     for (i = 0; i < 5; i++)
     {
         for (j = 0; j < 4; j++)
         {
-            scanf("%d",&class[i][j]);   
+            scanf("%d",&score[i][j]);   
         }
     }
-    for (i = 0; i < 5; i++)
+    result1 = score[0][0] + score[0][1] + score[0][2] + score[0][3];
+    result2 = score[1][0] + score[1][1] + score[1][2] + score[1][3];
+    result3 = score[2][0] + score[2][1] + score[2][2] + score[2][3];
+    result4 = score[3][0] + score[3][1] + score[3][2] + score[3][3];
+    result5 = score[4][0] + score[4][1] + score[4][2] + score[4][3];
+    if (result1/4 >= 80)
     {
-        for (j = 0; j < 4; j++)
-        {
-            
-        }
+        printf("pass\n");
+        pass++;
     }
+    else if (result1/4 < 80)
+    {
+        printf("fail\n");
+    }
+        if (result2/4 >= 80)
+    {
+        printf("pass\n");
+        pass++;
+    }
+    else if (result2/4 < 80)
+    {
+        printf("fail\n");
+    }
+        if (result3/4 >= 80)
+    {
+        printf("pass\n");
+        pass++;
+    }
+    else if (result3/4 < 80)
+    {
+        printf("fail\n");
+    }
+        if (result4/4 >= 80)
+    {
+        printf("pass\n");
+        pass++;
+    }
+    else if (result4/4 < 80)
+    {
+        printf("fail\n");
+    }
+    if (result5/4 >= 80)
+    {
+        printf("pass\n");
+        pass++;
+    }
+    else if (result5/4 < 80)
+    {
+        printf("fail\n");
+    }
+    printf("Successful : %d",pass);
+
+    return 0;
 }
