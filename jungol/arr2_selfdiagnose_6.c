8e2e11a8165c94be066c895b0c2401890e07e639
smartwe <sparklingwee@Gmail.com>
2021-03-22 09:38:51 +0900

    commit

--- jungol/arr2_selfdiagnose_6.c
+++ jungol/arr2_selfdiagnose_6.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+int main()
+{
+    int class[5][4];
+    int i, j;
+    int pass;  
+    int a, b;  
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            scanf("%d",&class[i][j]);   
+        }
+    }
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            
+        }
+    }
+}
