48fec873b47d1d9ae62f1ab80898c5a04db27fb6
smartwe <sparklingwee@Gmail.com>
2021-03-22 10:18:53 +0900

    commit

---
+++ jungol/arr2_selfdiagnose_6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int score[5][4];
+    int i, j;
+    int pass;
+    int total[5] = {0,};
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            scanf("%d",&score[i][j]);
+            total[i] = total[i] + score[i][j];   
+        }
+    }
+    for (i = 0; i < 5; i++)
+    {
+        if (total[i]/4 >= 80)
+        {
+            printf("pass\n");
+            pass++;
+        }
+        else if(total[i]/4 < 80)
+        {
+            printf("fail\n");
+        }   
+    }
+    printf("Successful : %d", pass);
+}
