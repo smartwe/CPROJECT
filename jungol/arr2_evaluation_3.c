cd1c3361919627ff8f7e07357503c6a104a27f93
smartwe <sparklingwee@Gmail.com>
2021-04-03 13:01:16 +0900

    commit

--- jungol/arr2_evaluation_3.c
+++ jungol/arr2_evaluation_3.c
@@ -3,10 +3,17 @@ int main()
 {
     int num1, num2;
     int i;
-    int arr[10]
+    int arr[10];
     scanf("%d",&num1);
     scanf("%d",&num2);
     arr[0] = num1;
     arr[1] = num2;
-    
+    for (i = 2; i < 10; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ",arr[i] % 10);
+    }
 }
