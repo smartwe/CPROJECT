4f6d9b96a4a6daf2e498c4871c167565dd5fb868
smartwe <sparklingwee@Gmail.com>
2021-04-03 11:39:00 +0900

    commit

--- jungol/arr2_evaluation_1.c
+++ jungol/arr2_evaluation_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
- 
+
+int arr[10];
+
 int main()
 {
-    int arr[10];
-    
+    int num, i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d",&num);
+        arr[num]++;
+    }
+    for (i = 0; i < 6; i++)
+    {
+        printf("%d : %d\n",i + 1, arr[i + 1]);
+    }
+    return 0;
 }
