7eb3cce5f20c576d6e81d8f2937932c2123ce086
smartwe <sparklingwee@Gmail.com>
2021-04-03 12:26:30 +0900

    commit

--- jungol/arr2_evaluation_2.c
+++ jungol/arr2_evaluation_2.c
@@ -2,12 +2,21 @@
 
 int main()
 {
-    int arr[100];
+    int arr[11];
     int i, score;
 
     for (i = 0; i < 100; i++)
     {
         scanf("%d",&score);
-        arr[score]++;
+        arr[score/10]++;
+        if (score == 0)
+        {
+            break;
+        }
+    }
+
+    for (i = 0; i < 100; i++)
+    {
+        printf("%d : %d person\n",i + 1,arr[i + 1]);
     }
 }
