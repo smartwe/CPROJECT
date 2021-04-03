e2192a226dd4e23a106b592001d92bb7ddef4a39
smartwe <sparklingwee@Gmail.com>
2021-04-03 13:12:38 +0900

    commit

--- jungol/arr2_evaluation_4.c
+++ jungol/arr2_evaluation_4.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int arr[4][3] = {{3, 5, 9}, {2, 11, 5}, {8, 30, 10}, {22, 5, 1}}
-    for (i = 0; i < ; i++)
+    int arr[4][3] = {{3, 5, 9}, {2, 11, 5}, {8, 30, 10}, {22, 5, 1}};
+    int i ,j;
+    int sum = 0;
+    for (i = 0; i < 4 ; i++)
     {
-        
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ",arr[i][j]);
+            sum = sum + arr[i][j];
+        }
+        printf("\n");
     }
+    printf("%d",sum);
 }
