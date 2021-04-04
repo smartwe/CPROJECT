6e6267fe5cc82df38fc7789f63065dd60c130250
smartwe <sparklingwee@Gmail.com>
2021-04-04 10:46:44 +0900

    commit

---
+++ jungol/arr2/arr2_evaluation_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[4][3];
+    int total[4];
+    int i, j, sum = 0;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%dclass? ", i + 1);
+        for (j = 0; j < 3; j++)
+        {
+            scanf(" %d",&arr[i][j]);
+        }
+    }
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            sum += arr[i][j];
+        }
+        total[i] = sum;
+        sum = 0;
+    }
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d\n",total[i]);
+    }
+}
