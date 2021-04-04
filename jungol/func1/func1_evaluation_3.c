9a13422c7e60be260fe499a05703c6dd4adbe2d3
smartwe <sparklingwee@Gmail.com>
2021-04-04 19:54:43 +0900

    commit

--- jungol/func1/func1_evaluation_3.c
+++ jungol/func1/func1_evaluation_3.c
@@ -8,8 +8,8 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i < n + 1 ; i++){
-        for(int j = 1; j < n + 1 ; j++){
+    for (int i = 1; i <= n ; i++){
+        for(int j = 1; j <= n ; j++){
             printf("%d ", rectanglenum(i, j));
         }
         puts("");
