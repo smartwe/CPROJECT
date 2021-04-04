09eb7b452f0c233f8f2f03bacc4d81896aded171
smartwe <sparklingwee@Gmail.com>
2021-04-04 19:16:53 +0900

    commit

---
+++ jungol/func1/func1_evaluation_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void gol()
+{
+    puts("@@@@@@@@@@");
+}
+int main()
+{
+    puts("first");
+    gol();
+    puts("second");
+    gol();
+    puts("third");
+    gol();
+
+    return 0;
+}
---
+++ jungol/func1/func1_evaluation_2.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+void gaus(int num)
+{
+    int sum = 0;
+    for (int i = num; i > 0; i--)
+    {
+        sum += i;
+    }
+    printf("%d",sum);
+}
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    gaus(num);
+}
+
+/*----------------------------------------------------------------
+#include <stdio.h>
+int gaus(int num) { return -~num*num>>1; }
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    printf("%d", gaus(num));
+}
+*/
+/*
+#include <stdio.h>
+int gaus(int num) {
+    if(num == 0)return 0;
+    return num + gaus(num - 1);
+}
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    printf("%d", gaus(num));
+}
+
+#include <stdio.h>
+int gaus(int num) { return num * (num + 1) / 2; }
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    printf("%d", gaus(num));
+}
+//*/
