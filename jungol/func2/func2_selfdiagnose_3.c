0a929185d9b42f03652a004c37af69db4bb9c702
smartwe <sparklingwee@gmail.com>
2021-04-10 11:41:21 +0900

    cmmit

--- jungol/func2/func2_selfdiagnose_3.c
+++ jungol/func2/func2_selfdiagnose_3.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 #include <math.h>
 
+int f(double c, double d) 
+{
+    if (fabs(c) < fabs(d))
+    {
+        return c;
+    }
+    else
+    {
+        return d;
+    }
+}
+int i(int a, int b)
+{
+    if (abs(a) < abs(b))
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
 int main()
 {
     int a, b;
-    scanf("%d",a);
-    scanf("%d",b);
-    int c, d;
-    scanf("%d",c);
-    scanf("%d",d);
-    
+    double c, d;
+    scanf("%d %d", &a, &b);
+    scanf("%lf %lf", &c, &d);
+    printf("%d",i(a, b));
+    printf("%.2lf",f(c, d));
+
+    return 0;
 }
