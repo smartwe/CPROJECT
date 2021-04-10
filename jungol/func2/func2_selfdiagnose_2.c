f5ad5cafe23d90fe546f4334e5de3102dc1eaacb
smartwe <sparklingwee@gmail.com>
2021-04-10 12:38:17 +0900

    commit

--- jungol/func2/func2_selfdiagnose_2.c
+++ jungol/func2/func2_selfdiagnose_2.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
-int date(int a. int b)
-{   
+bool a(int b)
+{
+    if (b <= 31)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool b(int c)
+{
+    if (c <= 30)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool c(int d)
+{
+    if (d <= 29)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int m, d;
+    scanf("%d",&m);
+    scanf("%d",&d);
     
 }
