93adf14aa7ef0624e74a6c1f9c9f1465f4ee0747
smartwe <sparklingwee@gmail.com>
2021-04-10 13:09:24 +0900

    commit

--- jungol/func2/func2_selfdiagnose_2.c
+++ jungol/func2/func2_selfdiagnose_2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 bool a(int b)
 {
-    if (b <= 31)
+    if (b <= 31 && 0 < b)
     {
         return true;
     }
@@ -12,7 +13,7 @@ bool a(int b)
 }
 bool b(int c)
 {
-    if (c <= 30)
+    if (c <= 30 && 0 < c)
     {
         return true;
     }
@@ -23,7 +24,7 @@ bool b(int c)
 }
 bool c(int d)
 {
-    if (d <= 29)
+    if (d <= 29 && 0 < d)
     {
         return true;
     }
@@ -37,5 +38,41 @@ int main()
     int m, d;
     scanf("%d",&m);
     scanf("%d",&d);
-    
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+    {
+        if (a(d))
+        {
+            printf("OK!");
+        }
+        else
+        {
+            printf("BAD!");
+        }
+    }
+    else if (m == 2)
+    {
+        if (c(d))
+        {
+            printf("OK!");
+        }
+        else
+        {
+            printf("BAD!");
+        }
+    }
+    else if (m == 4 || m == 6 || m == 9 || m == 11)
+    {
+        if (b(d))
+        {
+            printf("OK!");
+        }
+        else
+        {
+            printf("BAD!");
+        }
+    }
+    else
+    {
+        printf("BAD!");
+    }
 }
