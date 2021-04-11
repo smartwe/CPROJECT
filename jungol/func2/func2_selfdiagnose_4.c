990b18a8e63ade48148f58001f42e81c3c14614a
smartwe <sparklingwee@gmail.com>
2021-04-11 14:47:32 +0900

    commit

--- jungol/func2/func2_selfdiagnose_4.c
+++ jungol/func2/func2_selfdiagnose_4.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <math.h>
+int main()
+{
+    double area;
+    double r;
+    scanf("%lf",&area);
+    printf("%.2lf",sqrt(area / 3.14));
+
+    return 0;
+}
---
+++ jungol/func2/func2_selfdiagnose_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, c, tmp;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    double arr[3];
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+    for(int k = 0; k < 3; k++){
+        for (int i = 0; i < 2; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                }
+            }
+    }
+    printf("%.0lf %.0lf %.0lf", ceil(arr[2]), floor(arr[0]), round(arr[1]));
+}
