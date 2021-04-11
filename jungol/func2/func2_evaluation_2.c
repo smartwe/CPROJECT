4bd50ef54872c3bf18ae4fae9764b8374fa27bec
smartwe <sparklingwee@gmail.com>
2021-04-11 17:11:45 +0900

    commit

--- jungol/func2/func2_evaluation_2.c
+++ jungol/func2/func2_evaluation_2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    double num1, num2;
+    scanf("%lf %lf",&num1,&num2);
+
+    printf("%.0lf", fabs(ceil(sqrt(num1)) - ceil(sqrt(num2))) + 1);
+
+    return 0;
+}
---
+++ jungol/func2/func2_evaluation_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int arr[6];
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        sum += abs(arr[i]);
+    }
+}
