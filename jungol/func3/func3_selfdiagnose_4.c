cf35bf584bbe156b014f2068985e2d1a4197bad0
smartwe <sparklingwee@gmail.com>
2021-04-17 10:25:27 +0900

    comnit

---
+++ jungol/func3/func3_selfdiagnose_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int multi(int a)
+{
+    if(a <= 0) return 0;
+    return multi(a / 10) + (a % 10) * (a % 10);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", multi(n));
+    
+    return 0;
+}
