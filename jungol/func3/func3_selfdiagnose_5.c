348d132e01077ea94a283076aaef26da3d29bad9
smartwe <sparklingwee@gmail.com>
2021-04-17 11:44:24 +0900

    commit

--- jungol/func3/func3_selfdiagnose_5.c
+++ jungol/func3/func3_selfdiagnose_5.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
-void output(
+int N, arr[101];
+
+void output()
 {
-    
+	int i;
+	for (i = 1; i <= N; i++) 
+    {
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+void dice(int level)
+{
+	int i;
+
+	if (level > N) {
+		output();
+		return 0;
+	}
+
+	for (i = arr[level - 1]; i <= 6; i++)  
+    {
+		arr[level] = i;
+ 		dice(level + 1);
+    }
+}
+
+int main()
+{
+	scanf("%d", &N);
+	arr[0] = 1;
+
+	dice(1);
+	return 0;
 }
