be06c90de3b027fe6d0e7e0601792bd530b25990
smartwe <sparklingwee@gmail.com>
2021-04-18 14:34:45 +0900

    commit

---
+++ jungol/func3/func3_evaluation_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void func(int n)
+{
+    if(n <= 0)
+    {
+        return;
+    }
+    func(n - 2);
+    printf("%d ", n);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    func(n);
+
+    return 0;
+}
