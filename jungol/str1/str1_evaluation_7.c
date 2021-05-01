1d5f135e6f842894d0b657ed252991fbdbeb915b
smartwe <73765768+smartwe@users.noreply.github.com>
2021-05-01 13:56:33 +0900

    commit

--- jungol/str1/str1_evaluation_7.c
+++ jungol/str1/str1_evaluation_7.c
@@ -5,10 +5,12 @@ int main()
     char a[100];
     fgets(a, 100, stdin);
 
-    int i,j=1;
+    int i,j = 1;
     printf("%d. ", j++);
-    for(i=0;a[i]!=0;i++){
-        if(a[i] != ' ') {
+    for(i = 0; a[i] != 0; i++)
+    {
+        if(a[i] != ' ') 
+        {
             printf("%c", a[i]);
         }
         else{
