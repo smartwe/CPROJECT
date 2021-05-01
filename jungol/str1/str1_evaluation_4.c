0230d50498906c2f0c8bc8ec3b027c1b8f80aea6
smartwe <73765768+smartwe@users.noreply.github.com>
2021-05-01 11:08:24 +0900

    commit

--- jungol/str1/str1_evaluation_4.c
+++ jungol/str1/str1_evaluation_4.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char str[100];
+    char want;
+    int i2, len;
+    int cnt = 0;
+    scanf("%s %c", str, &want);
+    len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == want)
+        {
+            printf("%d", i);
+            
+            break;
+        }
+        else 
+        {
+            cnt++;
+        }
+    }
+    if (cnt == len)
+    {
+        printf("No");
+    }
+
+    return 0;
+
+}
