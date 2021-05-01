a6701c4855018992aa122aae0a7bc49e16b59d53
smartwe <73765768+smartwe@users.noreply.github.com>
2021-05-01 12:30:46 +0900

    commit

--- jungol/str1/str1_evaluation_5.c
+++ jungol/str1/str1_evaluation_5.c
@@ -5,4 +5,22 @@ int main()
 {
     char str[100];
     char str2[100];
+
+    scanf("%s", str);
+    scanf("%s", str2);
+
+    if (strlen(str) > strlen(str2))
+    {
+        printf("%d", strlen(str));
+    }
+    else if (strlen(str) < strlen(str2))
+    {
+        printf("%d", strlen(str2));
+    }
+    else
+    {
+        printf("%d", strlen(str));
+    }
+
+    return 0;
 }
