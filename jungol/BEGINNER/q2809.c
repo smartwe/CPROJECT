42d90bbdaf5cc5eba31b5cfb80f4fdcf136826c6
smartwe <73765768+smartwe@users.noreply.github.com>
2021-05-02 19:23:58 +0900

    commit

--- jungol/BEGINNER/q2809.c
+++ jungol/BEGINNER/q2809.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+    int n; 
+    double ren;
+    scanf("%d", &n);
+    ren = sqrt(n);
     
+    for (int i = 0; i < ren; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
 }
