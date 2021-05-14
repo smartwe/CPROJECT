1f555e23f19343c1c860ca8348e0b4e55f4d9206
smartwe <73765768+smartwe@users.noreply.github.com>
2021-05-14 22:53:50 +0900

    commit

--- baekjoon/10250.cpp
+++ baekjoon/10250.cpp
@@ -1,7 +1,19 @@
-#include <bits/stdc++.h>
-int main()
-{
-    int arr[100][100], n;
-    scanf("%d", &n);
-    
-}
+#include <bits/stdc++.h> 
+int main() 
+{ 
+    int t, h, w, n; 
+    scanf("%d", &t); 
+    for(int i = 0; i < t; i++) 
+    { 
+        scanf("%d %d %d", &h, &w, &n); 
+        if(n % h == 0) 
+        {
+            printf("%d%02d\n", h, n / h); 
+        }
+        else
+        {
+            printf("%d%02d\n", n % h, n / h + 1);  
+        }
+    }
+    return 0;
+} 
