c0c4d4b8f55c500f3a4a2568e97748fccf6b2545
smartwe <githubdev.smartwe@gmail.com>
2021-06-27 13:50:49 +0900

    commit

--- just/just.cpp
+++ just/just.cpp
@@ -1,13 +1,23 @@
-#include <stdio.h>
-int main()
-{
-    int a = 0, b = 1;
-    while (a + b <= 100)
-    {
-        printf("%d + %d = %d\n", a, b, a + b);
-        a = b;
-        b = a + b;
-    }
+#include<stdio.h>
+#include<algorithm>
+struct p{
+    int x,y;
+}a[104];
+//1. cmp 함수를 만든다.( 비교 함수)
+int cmp(p a,p b){
+    if(a.y==b.y)return (a.x>a.y);
+    return a.y>b.y;
+}
 
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&a[i].x,&a[i].y);
+    }
+    std::sort(a,a+n,cmp);
+    for(int i=0;i<n;i++){
+        printf("%d %d\n",a[i].x,a[i].y);
+    }
     return 0;
 }
